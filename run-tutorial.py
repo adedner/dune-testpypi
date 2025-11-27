073206b954d81d5b6f7588162b4bdf630fab1809
A Dedner <a.s.dedner@warwick.ac.uk>
2025-11-27 11:29:43 +0000

    also build notebooks

--- run-tutorial.py
+++ run-tutorial.py
@@ -34,13 +34,13 @@ tests = {
       "backuprestore.py",
       "uzawa-scipy.py",
       "evalues_laplace.py",
-      # "mixed_poisson.py",
+      "mixed_poisson.py",
     ],
     "extensions":[
       "chemical.py",
       "chimpl.py",
-      "euler.py",             # one of these
-      # "twophaseflow.py",      # does not terminate on MACOs
+      "euler.py", 
+      "twophaseflow.py",      # does not terminate on MACOs
       "vemdemo.py",
       "monolithicStokes.py",
       "fieldsplitStokes.py",
@@ -48,7 +48,7 @@ tests = {
       "cylinder.py"
     ]}
 
-disabled = ["3dexample.py", "limit.py"]
+disabled = ["3dexample.py"]
 
 # This block of code enables us to call the script from command line.
 def execute(process):
@@ -58,16 +58,29 @@ def execute(process):
         return [process,'skipped']
     print("START:",process,"...",flush=True)
     if process in disabled: return [process,'disabled']
-    if ".py" in process:
-        cmd = f'cd dune-fempy/doc ; PYTHONUNBUFFERED=1 python {process}'
-    else:
-        cmd = f'cd dune-fempy/doc ; PYTHONUNBUFFERED=1 jupyter nbconvert --execute --to notebook {process}'
+
+    # first run script
+    cmd = f'cd dune-fempy/doc ; PYTHONUNBUFFERED=1 python {process}'
+
     print("...",cmd,flush=True)
     start = time.time()
     ret = os.system(cmd)
     used = time.time() - start
     print("...",process,f"completed ({ret}) in {used}sec",flush=True)
+    
+    # on success also build notebook
+    if ret == 0:
+        notebook = process[:-3] # remove .py
+        notebook += "_nb.ipynb"
+        cmd = f'cd dune-fempy/doc ; make {process}'
+        print("...",cmd,flush=True)
+        start = time.time()
+        ret = os.system(cmd)
+        used = time.time() - start
+        print("...",process,f"completed ({ret}) in {used}sec",flush=True)
+
     return [process,ret,used]
+
 def build(tests,cores):
     tests = [t[:-3] for t in tests] # remove .py
     print("PRECOMPILE",flush=True)
@@ -78,21 +91,30 @@ def build(tests,cores):
     return ret
 
 if __name__ == "__main__":
-    examples = sys.argv[1]
-    cores = sys.argv[2]
+    examples = sys.argv[1]   # which subset of tests to execute
+                             # i.e. coreA, coreB, extensions
+    cores = sys.argv[2]      # how many cores are available to run scripts in parallel
+                             # used only for prebuild - actual scripts are
+                             # run in serial at the moment (see Pool below)
+
     print(f"Have {cores} cores available",flush=True)
 
+    # prebuild to populate dune-py if possible
     ret = build(tests[examples],cores)
-    if not ret == 0:
-        sys.exit(2)
+    # some precompilation failed but we are not yet treating this as an error
+    # if not ret == 0:
+    #     sys.exit(2)
 
+    # now run the actual script subset could be in parallel but currently isn't
     process_pool = multiprocessing.Pool(processes = 1)
     ret = process_pool.map(execute, tests[examples])
 
+    # print which script succeeded and which failed
     ret.sort()
     for r in ret:
         print(r)
-    # also print which scripts are not being run by comparing with all_tests
 
-    success = all([r[1]==0 or r[1]=="disabled" or r[1]=="skipped" for r in ret])
+    # exit with error if one or more scripts failed
+    success = all([r[1]==0 or r[1]=="disabled" or r[1]=="skipped"
+                   for r in ret])
     sys.exit(0 if success else 1)
